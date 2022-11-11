64e1a1c04dbb6398cba189d9acfa332fefa3f6c0
Jaehyun An <lvet0330@gmail.com>
2022-11-11 16:51:47 +0900

    페이지 연동 확인

--- src/main/java/com/example/kioskproject/controller/MainController.java
+++ src/main/java/com/example/kioskproject/controller/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String root(){
-        return "forward:/main";
+    @GetMapping(value = "/main")
+    public String main(){
+        return "main.html";
     }
 }
