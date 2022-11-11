67cd6d6810cd8ac36a0d2292d939bc2bbb64d65c
Jaehyun An <lvet0330@gmail.com>
2022-11-11 15:35:56 +0900

    java응용 프로젝트

---
+++ src/main/java/com/example/kioskproject/domain/Menu.java
@@ -0,0 +1,9 @@
+package com.example.kioskproject.domain;
+
+public class Menu {
+    private Long MenuID; // 메뉴 코드
+    private String MenuName; // 메뉴 이름
+    private int price; // 메뉴 가격
+    private String pic; // 메뉴 사진 (어떻게 넣을건지?)
+    //설명은 안넣어도 될 것 같아서 안넣었음.
+}
---
+++ src/main/java/com/example/kioskproject/domain/Stock.java
@@ -0,0 +1,8 @@
+package com.example.kioskproject.domain;
+
+public class Stock {
+    private Long StockID; //재고 코드
+    private String StockName; //재고 이름(재료 이름)
+    private int StockRest; //재고 갯수
+    //공급처 안넣어도 된다고 판단. 안넣었음.
+}
