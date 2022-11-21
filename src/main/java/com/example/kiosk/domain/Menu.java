df4db88f3bc877530a96a4184f992734f74560cc
Anjaehyun <lvet0330@gmail.com>
2022-11-21 18:00:38 +0900

    # domain 수정

--- src/main/java/com/example/kiosk/domain/Menu.java
+++ src/main/java/com/example/kiosk/domain/Menu.java
@@ -1,13 +1,58 @@
 package com.example.kiosk.domain;
 //메뉴에 대한 설명은 안넣어도 될 것 같아서 안넣었음.
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Entity
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long MenuID; // 메뉴 코드
+
+    @Setter
+    @Column(nullable = false)
     private String MenuName; // 메뉴 이름
+    @Setter
+    @Column(nullable = false)
     private int price; // 메뉴 가격
 //    private String pic; // 메뉴 사진 (어떻게 넣을건지?)
 
+    @CreatedDate
     private LocalDateTime orderedAt; //주문 시간
+
+    protected Menu() {
+    }
+
+    private Menu(Long menuID, String menuName, int price) {
+        MenuID = menuID;
+        MenuName = menuName;
+        this.price = price;
+    }
+
+    public static Menu of(Long menuID, String menuName, int price) {
+        return new Menu(menuID,menuName,price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Menu)) return false;
+        Menu menu = (Menu) o;
+        return MenuID.equals(menu.MenuID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(MenuID);
+    }
 }
--- src/main/java/com/example/kiosk/domain/Stock.java
+++ src/main/java/com/example/kiosk/domain/Stock.java
@@ -1,8 +1,45 @@
 package com.example.kiosk.domain;
 //교수님이 말씀하신 공급처 안넣어도 된다고 판단. 안넣었음.
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Entity
 public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long StockID; //재고 코드
-    private String StockName; //재고 이름(재료 이름)
-    private int StockRest; //재고 갯수
+
+    @Setter @Column(nullable = false) private String StockName; //재고 이름(재료 이름)
+    @Setter @Column(nullable = false) private int StockRest; //재고 갯수
+
+    protected Stock(){}
+
+    private Stock(Long stockID, String stockName, int stockRest) {
+        StockID = stockID;
+        StockName = stockName;
+        StockRest = stockRest;
+    }
+    public static Stock of (Long stockID, String stockName, int stockRest) {
+        return new Stock(stockID,stockName,stockRest);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Stock)) return false;
+        Stock stock = (Stock) o;
+        return StockID.equals(stock.StockID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(StockID);
+    }
 }
