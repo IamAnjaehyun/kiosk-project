424dd6519042373d65eb0ed7b353bf450485291a
Anjaehyun <lvet0330@gmail.com>
2022-11-21 16:27:45 +0900

    # Domain 수정

--- src/main/java/com/example/kiosk/domain/Menu.java
+++ src/main/java/com/example/kiosk/domain/Menu.java
@@ -1,9 +1,13 @@
 package com.example.kiosk.domain;
 //메뉴에 대한 설명은 안넣어도 될 것 같아서 안넣었음.
 
+import java.time.LocalDateTime;
+
 public class Menu {
     private Long MenuID; // 메뉴 코드
     private String MenuName; // 메뉴 이름
     private int price; // 메뉴 가격
-    private String pic; // 메뉴 사진 (어떻게 넣을건지?)
+//    private String pic; // 메뉴 사진 (어떻게 넣을건지?)
+
+    private LocalDateTime orderedAt; //주문 시간
 }
