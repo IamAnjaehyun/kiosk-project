d3817f2403601d0278422b6cc839d9b50afe892d
Anjaehyun <lvet0330@gmail.com>
2022-11-22 14:01:58 +0900

    # service 껍데기

---
+++ src/main/java/com/example/kiosk/service/BillService.java
@@ -0,0 +1,4 @@
+package com.example.kiosk.service;
+
+public class BillService {
+}
---
+++ src/main/java/com/example/kiosk/service/StockService.java
@@ -0,0 +1,11 @@
+package com.example.kiosk.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional
+@Service
+public class StockService {
+
+}
