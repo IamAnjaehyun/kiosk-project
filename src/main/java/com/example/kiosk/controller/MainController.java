899e30303436c1cc11a6e4da5bf043685d4501bc
Anjaehyun <lvet0330@gmail.com>
2022-11-23 11:52:23 +0900

    # 주석추가

--- src/main/java/com/example/kiosk/controller/MainController.java
+++ src/main/java/com/example/kiosk/controller/MainController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping(value = "/order")
+    @RequestMapping(value = "/order") //고객주문페이지
     public String main(){
         return "user_order.html";
     }
 
-    @RequestMapping(value = "/admin")
+    @RequestMapping(value = "/admin") //관리자 제어 페이지
     public String admin(){
         return "admin_menu.html";
     }
