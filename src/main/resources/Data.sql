b584f463b119856c520f94f5183a9b8302f53e7a
Anjaehyun <lvet0330@gmail.com>
2022-11-26 12:31:44 +0900

    # html 수정

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -3,3 +3,26 @@ insert into menu (menuid, menu_name, price) values (2, 'test1', 5000);
 insert into bill (OrderId) values (3);
 
 insert into stock (stockid, stock_name, stock_rest) values (4, 'test1', 4);
+
+create table kiosk.bill
+(
+    orderid bigint auto_increment
+        primary key
+);
+
+create table kiosk.menu
+(
+    menuid    bigint auto_increment
+        primary key,
+    menu_name varchar(255) not null,
+    price     int          not null
+);
+
+create table kiosk.stock
+(
+    stockid    bigint auto_increment
+        primary key,
+    stock_name varchar(255) not null,
+    stock_rest int          not null
+);
+
