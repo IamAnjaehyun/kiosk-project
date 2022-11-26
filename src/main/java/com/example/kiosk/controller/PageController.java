bbcecf66d301dcd6136cfce40ac09e8efab830a8
Anjaehyun <lvet0330@gmail.com>
2022-11-26 16:26:14 +0900

    # ㅎㅇㅌ!

--- src/main/java/com/example/kiosk/controller/PageController.java
+++ src/main/java/com/example/kiosk/controller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+public class PageController {
     @RequestMapping(value = "/index") //인덱스 페이지
     public String main(){
         return "index";
