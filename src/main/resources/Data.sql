12ee20886733eb57d9c760ae86f194ca46131c6b
Anjaehyun <lvet0330@gmail.com>
2022-11-26 22:55:47 +0900

    # sql fix

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,6 +1,16 @@
-insert into menu (menuid, menu_name, price) values (2, 'test1', 5000);
+insert into menu (menuID, menu_name, price, menu_count) values (1,'test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test2', 5002, 2);
+insert into menu (menu_name, price, menu_count) values ('test3', 5003, 3);
+insert into menu (menu_name, price, menu_count) values ('test4', 5004, 4);
+insert into menu (menu_name, price, menu_count) values ('test5', 5005, 5);
+insert into menu (menu_name, price, menu_count) values ('test6', 5006, 6);
+insert into menu (menu_name, price, menu_count) values ('test7', 5007, 7);
+insert into menu (menu_name, price, menu_count) values ('test8', 5007, 7);
+insert into menu (menu_name, price, menu_count) values ('test9', 5007, 7);
+insert into menu (menu_name, price, menu_count) values ('test10', 5088, 7);
+insert into menu (menu_name, price, menu_count) values ('test11', 5088, 7);
+
 
 insert into bill (OrderId) values (3);
 
-insert into stock (stockid, stock_name, stock_rest) values (4, 'test1', 4);
 
