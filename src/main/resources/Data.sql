6e410d8e30444361dbb1be9767588586f286e0bc
Anjaehyun <lvet0330@gmail.com>
2022-11-28 21:24:58 +0900

    # db 확인+jaws 확인용

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,15 +1,15 @@
 insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test2', 5002, 2);
-insert into menu (menu_name, price, menu_count) values ('test3', 5003, 3);
-insert into menu (menu_name, price, menu_count) values ('test4', 5004, 4);
-insert into menu (menu_name, price, menu_count) values ('test5', 5005, 5);
-insert into menu (menu_name, price, menu_count) values ('test6', 5006, 6);
-insert into menu (menu_name, price, menu_count) values ('test7', 5007, 7);
-insert into menu (menu_name, price, menu_count) values ('test8', 5007, 7);
-insert into menu (menu_name, price, menu_count) values ('test9', 5007, 7);
-insert into menu (menu_name, price, menu_count) values ('test10', 5088, 7);
-insert into menu (menu_name, price, menu_count) values ('test11', 5088, 7);
-
+# insert into menu (menu_name, price, menu_count) values ('test2', 5002, 2);
+# insert into menu (menu_name, price, menu_count) values ('test3', 5003, 3);
+# insert into menu (menu_name, price, menu_count) values ('test4', 5004, 4);
+# insert into menu (menu_name, price, menu_count) values ('test5', 5005, 5);
+# insert into menu (menu_name, price, menu_count) values ('test6', 5006, 6);
+# insert into menu (menu_name, price, menu_count) values ('test7', 5007, 7);
+# insert into menu (menu_name, price, menu_count) values ('test8', 5007, 7);
+# insert into menu (menu_name, price, menu_count) values ('test9', 5007, 7);
+# insert into menu (menu_name, price, menu_count) values ('test10', 5088, 7);
+# insert into menu (menu_name, price, menu_count) values ('test11', 5088, 7);
+# 10개 넘어서 db 오류생기나 확인차 주석
 
 insert into bill (OrderId) values (3);
 
