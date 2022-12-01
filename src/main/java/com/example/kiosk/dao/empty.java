a103caa7150dcca217564a50e79e12a620269d7c
Anjaehyun <lvet0330@gmail.com>
2022-12-01 23:02:06 +0900

    # 최최종

--- src/main/java/com/example/kiosk/dao/empty.java
+++
@@ -1,5 +0,0 @@
-package com.example.kiosk.dao;
-
-public class empty {
-    //데이터 조회 or 조작
-}
--- src/main/java/com/example/kiosk/domain/Repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kiosk.domain.Repository;
-
-import com.example.kiosk.domain.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Menu, Long> {
-
-}
