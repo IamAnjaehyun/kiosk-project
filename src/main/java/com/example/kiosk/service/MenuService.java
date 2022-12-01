41838fe13ae659dba11437aa4ab12ddfe003c0dc
IamJaehyun <lvet0330@gmail.com>
2022-12-01 18:52:31 +0900

    # fix service

--- src/main/java/com/example/kiosk/service/MenuService.java
+++ src/main/java/com/example/kiosk/service/MenuService.java
@@ -2,11 +2,13 @@ package com.example.kiosk.service;
 
 import com.example.kiosk.Repository.MenuRepository;
 import com.example.kiosk.domain.Menu;
+import com.example.kiosk.dto.MenuDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional //db 수정하다 되돌리기 가능
@@ -17,13 +19,18 @@ public class MenuService {
     @Autowired
     private MenuRepository menuRepository;
 
+    public MenuService(MenuRepository menuRepository){
+        this.menuRepository = menuRepository;
+    }
+
     public List<Menu> selectMenu() {
         List<Menu> menuList = menuRepository.findAll();
         return menuList;
     }
 
-    public void commit(Menu menu){
-        menuRepository.save(menu);
+    @Transactional
+    public Long savePost(MenuDto menuDto){
+        return menuRepository.save(menuDto.toEntity()).getMenuID();
     }
 
     public void menuDelete(Long menuID){
