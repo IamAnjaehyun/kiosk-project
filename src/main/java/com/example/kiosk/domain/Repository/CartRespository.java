7f891c56d46e0a86b89369bf8f4b1077b2c5dbc3
Anjaehyun <lvet0330@gmail.com>
2022-12-02 19:41:03 +0900

    # Bill  - > Cart

--- src/main/java/com/example/kiosk/domain/Repository/CartRespository.java
+++ src/main/java/com/example/kiosk/domain/Repository/CartRespository.java
@@ -1,7 +1,7 @@
 package com.example.kiosk.domain.Repository;
 
-import com.example.kiosk.domain.Bill;
+import com.example.kiosk.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillRepository extends JpaRepository<Bill, Long> {
+public interface CartRespository extends JpaRepository<Cart, Long> {
 }
