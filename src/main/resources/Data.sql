1e96d99dc8ed7bd0297c1db4eb367c8b547ac759
Anjaehyun <lvet0330@gmail.com>
2022-12-02 19:41:25 +0900

    # .

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,4 +1,11 @@
 insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
+insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
 
 insert into menu (menu_name, price, menu_count) values ('whywhywhywhwywhwy', 9999, 1111);
 # 10개 넘어서 db 오류생기나 확인차 주석
