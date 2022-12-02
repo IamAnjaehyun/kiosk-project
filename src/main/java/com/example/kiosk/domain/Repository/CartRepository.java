660200111b51fce9fa9247e2e2c55f3bb2877ee1
Anjaehyun <lvet0330@gmail.com>
2022-12-02 19:41:54 +0900

    # 오타수정

--- src/main/java/com/example/kiosk/domain/Repository/CartRepository.java
+++ src/main/java/com/example/kiosk/domain/Repository/CartRepository.java
@@ -3,5 +3,5 @@ package com.example.kiosk.domain.Repository;
 import com.example.kiosk.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRespository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
