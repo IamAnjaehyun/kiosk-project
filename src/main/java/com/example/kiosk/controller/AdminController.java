7a9e867ff24f9440464392dc0fe6c6483b05b57c
IamJaehyun <lvet0330@gmail.com>
2022-12-05 00:02:04 +0900

    # img 추가 일시중단

--- src/main/java/com/example/kiosk/controller/AdminController.java
+++ src/main/java/com/example/kiosk/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.example.kiosk.controller;
 
+import com.example.kiosk.domain.Menu;
 import com.example.kiosk.dto.MenuDto;
 import com.example.kiosk.service.MenuService;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,59 +20,64 @@ public class AdminController {
     @Autowired
     private MenuService menuService;
 
-    public AdminController(MenuService menuService){
-        this.menuService=menuService;
+    public AdminController(MenuService menuService) {
+        this.menuService = menuService;
     }
 
     @GetMapping("/")
-    public String list(Model model){
+    public String list(Model model) {
         List<MenuDto> menuDtoList = menuService.getMenulist();
-        model.addAttribute("menuList",menuDtoList);
+        model.addAttribute("menuList", menuDtoList);
 
         return "admin_menu";
     }
 
     @GetMapping("/post")
-    public String write(){
+    public String write() {
         return "write";
     }
 
     @PostMapping("/post")
-    public String write(MenuDto menuDto){
+    public String write(MenuDto menuDto) {
         menuService.savePost(menuDto);
         return "redirect:/";
     }
 
     @GetMapping("/post/{menuID}")
-    public String detail(@PathVariable("menuID") Long menuID, Model model){
+    public String detail(@PathVariable("menuID") Long menuID, Model model) {
         MenuDto menuDto = menuService.getMenu(menuID);
 
-        model.addAttribute("menuDto",menuDto);
+        model.addAttribute("menuDto", menuDto);
         return "detail";
     }
 
 
     @GetMapping("/post/edit/{menuID}")
-    public String edit(@PathVariable("menuID") Long menuID, Model model){
+    public String edit(@PathVariable("menuID") Long menuID, Model model) {
         MenuDto menuDto = menuService.getMenu(menuID);
 
-        model.addAttribute("menuDto",menuDto);
+        model.addAttribute("menuDto", menuDto);
         return "update";
     }
 
     @PutMapping("/post/edit/{menuID}")
-    public String update(MenuDto menuDto){
+    public String update(MenuDto menuDto) {
         menuService.savePost(menuDto);
         return "redirect:/";
     }
 
     @DeleteMapping("/post/{menuID}")
-    public String delete(@PathVariable("menuID") Long menuID){
+    public String delete(@PathVariable("menuID") Long menuID) {
         menuService.deletePost(menuID);
 
         return "redirect:/";
     }
 //    보고 사진추가 기능 구현하기
 //    https://velog.io/@rladuswl/8-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%8A%A4%ED%84%B0%EB%94%94-%EC%87%BC%ED%95%91%EB%AA%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%83%81%ED%92%88-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C%EC%99%80-thymeleaf
-    
+
+//    @PostMapping("/post/img/{menuID}")
+//    public String itemSave(@PathVariable("menuID") Menu menu, MultipartFile imgFile) throws Exception {
+//        menuService.saveMenu(menu, imgFile);
+//        return "redirect:/";
+//    }
 }
--- src/main/java/com/example/kiosk/service/MenuService.java
+++ src/main/java/com/example/kiosk/service/MenuService.java
@@ -6,11 +6,14 @@ import com.example.kiosk.dto.MenuDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Transactional //db 수정하다 되돌리기 가능
 @Service
@@ -65,6 +68,32 @@ public class MenuService {
     public void deletePost(Long menuID){
         menuRepository.deleteById(menuID);
     }
+
+    // 상품 등록
+//    public void saveMenu(Menu menu, MultipartFile imgFile) throws Exception {
+//
+//        String oriImgName = imgFile.getOriginalFilename();
+//        String imgName = "";
+//
+//        String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/files/";
+//
+//        // UUID 를 이용하여 파일명 새로 생성
+//        // UUID - 서로 다른 객체들을 구별하기 위한 클래스
+//        UUID uuid = UUID.randomUUID();
+//
+//        String savedFileName = uuid + "_" + oriImgName; // 파일명 -> imgName
+//
+//        imgName = savedFileName;
+//
+//        File saveFile = new File(projectPath, imgName);
+//
+//        imgFile.transferTo(saveFile);
+//
+//        menu.setImgName(imgName);
+//        menu.setImgPath("/files/" + imgName);
+//
+//        menuRepository.save(menu);
+//    }
 }
 
 
