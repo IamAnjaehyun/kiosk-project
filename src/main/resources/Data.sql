87f7ab0ff4fdcd7db91b59d5b0ebda6c850eda71
IamJaehyun <lvet0330@gmail.com>
2022-12-04 15:55:31 +0900

    # ? bill 삭제 no2

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,15 +1,4 @@
 insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
-insert into menu (menu_name, price, menu_count) values ('test1', 5001, 1);
 
-insert into menu (menu_name, price, menu_count) values ('whywhywhywhwywhwy', 9999, 1111);
-# 10개 넘어서 db 오류생기나 확인차 주석
-
-insert into bill (OrderId) values (3);
 
 
