f4f966a36a8af0a80d88fe7d8eeee736df7ca4de
IamJaehyun <lvet0330@gmail.com>
2022-12-04 23:07:57 +0900

    # 사진추가 기능구현 위한 링크 추가
    #

--- src/main/java/com/example/kiosk/controller/AdminController.java
+++ src/main/java/com/example/kiosk/controller/AdminController.java
@@ -70,5 +70,7 @@ public class AdminController {
 
         return "redirect:/";
     }
+//    보고 사진추가 기능 구현하기
+//    https://velog.io/@rladuswl/8-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%8A%A4%ED%84%B0%EB%94%94-%EC%87%BC%ED%95%91%EB%AA%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%83%81%ED%92%88-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C%EC%99%80-thymeleaf
     
 }
