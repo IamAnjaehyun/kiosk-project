f8015bee5f0a53dca41bfd1ed24d435c6e1ce6d3
IamJaehyun <lvet0330@gmail.com>
2022-12-06 03:25:14 +0900

    # File Domain 삭제

--- src/main/java/com/example/kiosk/domain/Repository/FileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.kiosk.domain.Repository;
-
-import com.example.kiosk.domain.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {}
