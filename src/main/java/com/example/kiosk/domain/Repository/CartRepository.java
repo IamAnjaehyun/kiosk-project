cd37037615d846f2b178abb4e67a4b6289f80798
IamJaehyun <lvet0330@gmail.com>
2022-12-06 20:05:02 +0900

    # 날아간 commit 복구 - repo/service/md5

--- src/main/java/com/example/kiosk/domain/Repository/CartRepository.java
+++ src/main/java/com/example/kiosk/domain/Repository/CartRepository.java
@@ -1,7 +1,7 @@
-package com.example.kiosk.domain.Repository;
-
-import com.example.kiosk.domain.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
+//package com.example.kiosk.domain.Repository;
+//
+//import com.example.kiosk.domain.Cart;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CartRepository extends JpaRepository<Cart, Long> {
+//}
