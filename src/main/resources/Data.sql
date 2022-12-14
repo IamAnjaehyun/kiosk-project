af72d19d0b472d11c3839d216d9a2a045dd97b14
Anjaehyun <lvet0330@gmail.com>
2022-12-14 10:30:21 +0900

    # yaml 수정
    db초기화 시키려고

--- src/main/resources/Data.sql
+++
@@ -1,4 +0,0 @@
-insert into menu (menu_name, price, menu_count, file_path) values ('치킨', 5001, 1,'files/1e2f97599ef772b16169f0935429cef5');
-insert into menu (menu_name, price, menu_count, file_path) values ('피자', 5001, 1,'files/1e2f97599ef772b16169f0935429cef5');
-insert into menu (menu_name, price, menu_count, file_path) values ('햄버거', 5001, 1,'files/1e2f97599ef772b16169f0935429cef5');
-insert into menu (menu_name, price, menu_count, file_path) values ('콜라', 5001, 1,'files/1e2f97599ef772b16169f0935429cef5');
